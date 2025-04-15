607abe3a2960d6e257d38b02c1d427aff7ae2b0a
Ryan Hangralim <ryanhangralim@gmail.com>
2025-04-15 08:47:03 +0800

    Initial Commit

---
+++ Halal Food Map/Halal_Food_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Halal_Food_MapApp.swift
+//  Halal Food Map
+//
+//  Created by Ryan Hangralim on 15/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Halal_Food_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
