2be3fdbf1e675435a97aa812bdc5561caab851a7
Ryan Hangralim <ryanhangralim@gmail.com>
2025-04-15 11:05:59 +0800

    feat: Added dummy restaurant data

--- Halal Food Map/ContentView.swift
+++ Halal Food Map/ContentView.swift
@@ -15,9 +15,22 @@ struct ContentView: View {
     let academy = CLLocationCoordinate2D(latitude: -8.737300, longitude: 115.175790)
     
     var body: some View {
-        Map(position: $camera) {
-            Marker("Academy", monogram: Text("ADA"), coordinate: academy)
-                .tint(.green)
+//        Map(position: $camera) {
+//            Marker("Academy", monogram: Text("ADA"), coordinate: academy)
+//                .tint(.green)
+//        }
+        NavigationView {
+            List(Restaurant.dummyData) { restaurant in
+                VStack(alignment: .leading) {
+                    Text(restaurant.name)
+                        .font(.headline)
+                    Text("\(restaurant.category) • \(restaurant.distance)")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+                .padding(.vertical, 4)
+            }
+            .navigationTitle("Halal Restaurants")
         }
     }
 }
--- Halal Food Map/Model/Restaurant.swift
+++ Halal Food Map/Model/Restaurant.swift
@@ -13,6 +13,7 @@ struct Restaurant: Identifiable {
     var name: String
     var category: String
     var distance: String
+    var address: String
     var avgRating: Double
     var totalReviews: Int
     var isCertified: Bool
@@ -22,4 +23,15 @@ struct Restaurant: Identifiable {
     var coordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     }
+}
+
+extension Restaurant {
+    static let dummyData: [Restaurant] = [
+        Restaurant(name: "Amanaia", category: "Indonesian Restaurant", distance: "60 m", address: "Jl. Kediri, Tuban, Kec. Kuta, Bali, 80361" ,avgRating: 4.5, totalReviews: 2, isCertified: true, latitude: -8.736982293243265, longitude: 115.17502036325229),
+        Restaurant(name: "Warung Sate Madura", category: "Satay Restaurant", distance: "160 m", address: "Jl Kediri No K8, Tuban, Kec. Kuta, Kabupaten Badung, Bali 80361", avgRating: 4.2, totalReviews: 10, isCertified: false, latitude: -8.737620829020402, longitude: 115.17731537963436),
+        Restaurant(name: "Richeese Factory Kuta", category: "Fast food", distance: "500 m", address: "Jl. Raya Kuta No.131B, Kuta, Kec. Kuta, Kabupaten Badung, Bali 80361", avgRating: 4.0, totalReviews: 1000, isCertified: true, latitude: -8.735108903350465, longitude: 115.1778232661139),
+        Restaurant(name: "Holland Bakery Tuban Kuta", category: "Bakery", distance: "550 m", address: "Jl. Raya Tuban No.99, Tuban, Kec. Kuta, Kabupaten Badung, Bali 80361", avgRating: 4.5, totalReviews: 200, isCertified: true, latitude: -8.739601464969834, longitude: 115.17861790210725),
+        Restaurant(name: "Warung Nasi Ayam Bu Oki", category: "Balinese Restaurant", distance: "1.0 km", address: "Jl. Raya Tuban No.3, Tuban, Kec. Kuta, Kabupaten Badung, Bali 80361", avgRating: 4.4, totalReviews: 1700, isCertified: false, latitude: -8.743698362140202, longitude: 115.17918582835517),
+        Restaurant(name: "Nasi Pedas Ibu Andika", category: "Rice Restaurant", distance: "1.4 km", address: "Jl. Raya Kuta No.100, Kuta, Kec. Kuta, Kabupaten Badung, Bali 80361", avgRating: 4.1, totalReviews: 4000, isCertified: false, latitude: -8.726735738329467, longitude: 115.1766869118225)
+    ]
 }
